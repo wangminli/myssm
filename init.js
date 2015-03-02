d40b35711484626d60431381b54363e0baba6c90
wangminli <wongminli@163.com>
2015-03-02 22:36:00 +0800

    update init.js

--- init.js
+++ init.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
 	$dl.find("h3").next().hide();
 	$dl.find("h3").click(function(){
 		$(this).next().addClass(".clicked");
-		var $other = $dl.find("h3").next().not(".clicked");
-		$other.slideUp();
+		$dl.find("h3").next().each(function(){
+			if(!$(this).hasClass("clicked")){
+				$(this).next().slideUp();
+			};
+		});
 		$(this).next().slideToggle();
 		$(this).next().removeClass(".clicked");
 	});
 });
-
