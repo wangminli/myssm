8367361a4e507a343150f9592ae69eb0f954f19f
wangminli <wongminli@163.com>
2015-03-02 21:36:40 +0800

    udpate

--- init.js
+++ init.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 	var $dl = $("#dl");	
 	$dl.find("h3").next().hide();
 	$dl.find("h3").click(function(){
+		$(this).next().addClass(".clicked");
+		var $other = $dl.find("h3").next().not(".clicked");
+		//$other.slideUp();
 		$(this).next().slideToggle();
+		$(this).next().removeClass(".clicked");
 	});
 });
 
