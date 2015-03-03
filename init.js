c473dca2c86ad9db4aed1f197dbd83098527fee4
wangminli <wongminli@163.com>
2015-03-03 11:02:19 +0800

    update init.js

--- init.js
+++ init.js
@@ -2,13 +2,12 @@ $(document).ready(function(){
 	var $dl = $("#dl");	
 	$dl.find("h3").next().hide();
 	$dl.find("h3").click(function(){
-		$(this).next().addClass(".clicked");
-		$dl.find("h3").next().each(function(){
-			if(!$(this).hasClass("clicked")){
-				$(this).next().slideUp();
-			};
+		var that = this;
+		$dl.find("h3").each(function(){
+			if(that != this){  //防止关闭自身时出现“先关闭又打开”的情况
+				$(this).next().slideUp();	
+			}
 		});
 		$(this).next().slideToggle();
-		$(this).next().removeClass(".clicked");
 	});
 });
