cfa13768eb79d3c366a4b14786146d7c9fee84d0
wangminli <wongminli@163.com>
2017-12-14 09:55:01 +0800

    上一次没提交全~！第一次初始化，最大化保持原生，仅添加了generatorconfig.xml和对应pom，已过滤idea配置文件

---
+++ ssm-manage/ssm-manage-web/src/test/java/com/youmeek/ssm/SSMTest.java
@@ -0,0 +1,29 @@
+package com.youmeek.ssm;
+
+import com.youmeek.ssm.manage.pojo.SysUser;
+import com.youmeek.ssm.manage.service.SysUserService;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring/applicationContext*.xml"})
+@Ignore
+public class SSMTest {
+
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+	
+	@Resource
+	private SysUserService sysUserService;
+
+	@Test
+	public void test1() {
+		//SysUser sysUser = sysUserService.getById(1L);
+		//logger.info("--------------------------------" + sysUser.toString());
+	}
+}
