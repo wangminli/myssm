be893840452b269cf0cb3f978c358217bc8d2900
wangminli <wongminli@163.com>
2017-12-20 14:12:02 +0800

    统一common常用枚举类型，避免不必要的线上异常

---
+++ ssm-commons/src/main/java/com/wml/constant/Constants.java
@@ -0,0 +1,11 @@
+package com.wml.constant;
+
+/** Description: 各种常用常量
+ * Created by wangminli on 2017/12/20.
+ */
+public class Constants {
+    public  static final String YES = "yes";
+    public  static final String NO = "no";
+    public  static final String SUCCESS = "success";
+    public  static final String FAIL = "fail";
+}
---
+++ ssm-commons/src/main/java/com/wml/constant/WeekEnum.java
@@ -0,0 +1,18 @@
+package com.wml.constant;
+
+/**
+ * Decription: 星期/周
+ * Created by WangMinli on 2017/12/20.
+ */
+public enum WeekEnum {
+    MONDAY(1),
+    DUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAYS(4),
+    FRIDAYS(5),
+    SATURDAYS(6),
+    SUNDAYS(7);
+
+    WeekEnum(int index) {
+    }
+}
