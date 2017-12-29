6e09a6651a50d92cf3342e24fe7d7017ec871318
wangminli <wongminli@163.com>
2017-12-29 16:43:57 +0800

    测试jedis的五种类型的操作

--- ssm-manage/ssm-manage-web/src/test/java/com/wml/test/JedisTest.java
+++ ssm-manage/ssm-manage-web/src/test/java/com/wml/test/JedisTest.java
@@ -19,8 +19,7 @@ public class JedisTest {
     public void testJedis(){
         Jedis jedis = new Jedis(ip, 6379);
         jedis.set("name", "zhangsan");
-        String name = jedis.get("name");
-        System.out.println(name);
+        System.out.println(jedis.get("name"));
         jedis.close();
     }
     /**
@@ -40,8 +39,34 @@ public class JedisTest {
         Jedis jedis = null;
         try {
             jedis = jedisPool.getResource();
+            // 测试String
             jedis.set("name", "lisi");
             System.out.println(jedis.get("name"));
+
+            // 测试hash
+            jedis.hset("myhash", "name", "jefferson");
+            jedis.hset("myhash", "age", "25");
+            System.out.println(jedis.hget("myhash", "age"));
+
+            // 测试list
+            jedis.rpush("mylist", "1");
+            jedis.rpush("mylist", "2");
+            jedis.rpush("mylist", "3");
+            System.out.println(jedis.lrange("mylist", 0, -1));  // [1, 2, 3]
+
+            // 测试set
+            jedis.sadd("myset", "a");
+            jedis.sadd("myset", "b");
+            jedis.sadd("myset", "a");
+            System.out.println(jedis.smembers("myset")); // [b, a]
+
+            // 测试zset
+            jedis.zadd("myzset", 2, "wangminli");
+            jedis.zadd("myzset", 7, "lisi");
+            jedis.zadd("myzset", 11, "wangwu");
+            System.out.println(jedis.zscore("myzset", "lisi"));
+            System.out.println(jedis.zrange("myzset", 0, -1));
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
